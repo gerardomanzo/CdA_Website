d1e5b07179a07a16b1507b136e042784df9722d2
Francesco Manzo <francescomanzo@outlook.com>
2016-10-27 20:55:27 +0200

    List storico

--- areaprivata.php
+++ areaprivata.php
@@ -59,44 +59,68 @@
 					<?php
 				}
 				else
-				{?>
-					<img src="./images/unlocked.png" alt="" class="lock">
-					<div class="areaprivata">
-						<h1 class="titolo" style="margin-top: 50px">MEN&Ugrave; AREA PRIVATA</h1>
-						<ul>
-							<li><a href="">Inserisci News / Annuncio</a></li>
-							<li><a href="">Inserisci uno Storico</a></li>
-							<li><a href="logout.php">LOGOUT</a></li>
-						</ul>
-					</div>
-					<!-- <div class="form">
-						<h1 class="titolo">Pubblica una news</h1>
-						<form method="POST" action="addAnnuncio.php">
-							<input class="input" name="titolo_news" type="text" placeholder="Titolo news">
-							<textarea class="textarea" name="messaggio_news" maxlength="500" placeholder="Messaggio (max 500 caratteri)" noresize></textarea>
-							<input class="button" type="submit" value="Pubblica news">
-						</form>
-					</div>
+				{
+					if(!isset($_GET["form"]))
+					{?>
+						<img src="./images/unlocked.png" alt="" class="lock">
+						<div class="areaprivata">
+							<h1 class="titolo" style="margin-top: 50px">MEN&Ugrave; AREA PRIVATA</h1>
+							<ul>
+								<li><a href="areaprivata.php?form=1">Inserisci News / Annuncio</a></li>
+								<li><a href="areaprivata.php?form=2">Inserisci uno Storico</a></li>
+								<li><a href="logout.php">LOGOUT</a></li>
+							</ul>
+						</div>
+						<?php
+					}
+					else if($_GET["form"] == "1")
+					{?>
+						<div class="form">
+							<h1 class="titolo">Pubblica una news</h1>
+							<form method="POST" action="addAnnuncio.php">
+								<input class="input" name="titolo_news" type="text" placeholder="Titolo news">
+								<textarea class="textarea" name="messaggio_news" maxlength="500" placeholder="Messaggio (max 500 caratteri)" noresize></textarea>
+								<input class="button" type="submit" value="Pubblica news">
+							</form>
+						</div>
 
-					<div class="form">
-						<h1 class="titolo">Crea un evento</h1>
-						<form method="POST" action="addEvento.php">
-							<input class="input" name="titolo_evento" type="text"  placeholder="Titolo evento">
-							<input class="input" name="luogo_evento" type="text"  placeholder="Luogo evento">
-							<input class="input" name="data_evento" type="text"  placeholder="Data evento">
-							<input class="button" class="input" type="submit" value="Crea evento">
-						</form>
-					</div>
-
-					<div class="form">
-						<h1 class="titolo">Crea una storia</h1>
-						<form enctype="multipart/form-data" action="addStoria.php" method="POST">
-							<input name="file[]" type="file" id="file" multiple>
-							<input class="button" type="submit" value="Carica immagini" name="submit" id="upload">
-						</form>
-					</div> -->
-					<?php
-				}?>
+						<div class="form">
+							<h1 class="titolo">Crea un evento</h1>
+							<form method="POST" action="addEvento.php">
+								<input class="input" name="titolo_evento" type="text"  placeholder="Titolo evento">
+								<input class="input" name="luogo_evento" type="text"  placeholder="Luogo evento">
+								<input class="input" name="data_evento" type="text"  placeholder="Data evento">
+								<input class="button" class="input" type="submit" value="Crea evento">
+							</form>
+						</div>
+						<?php
+					}
+					else if($_GET["form"] == "2")
+					{?>
+						<div class="form">
+							<h1 class="titolo">Crea una storia</h1>
+							<form enctype="multipart/form-data" action="addStoria.php" method="POST">
+								<input name="file[]" type="file" id="file" multiple>
+								<input class="button" type="submit" value="Carica immagini" name="submit" id="upload">
+							</form>
+						</div>
+						<?php
+					}
+					else
+					{?>
+						<img src="./images/unlocked.png" alt="" class="lock">
+						<div class="areaprivata">
+							<h1 class="titolo" style="margin-top: 50px">MEN&Ugrave; AREA PRIVATA</h1>
+							<ul>
+								<li><a href="areaprivata.php?form=1">Inserisci News / Annuncio</a></li>
+								<li><a href="areaprivata.php?form=2">Inserisci uno Storico</a></li>
+								<li><a href="logout.php">LOGOUT</a></li>
+							</ul>
+						</div>
+						<?php						
+					}
+				}
+			?>
 		</section>
 	</div>
 
---
+++ storico.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+	<head>
+		<title>Consorzio dell'Agro ONLUS</title>
+		<link rel="stylesheet" type="text/css" href="css/style2.css">
+		<link href="https://fonts.googleapis.com/css?family=Pontano+Sans" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Coming+Soon" rel="stylesheet">
+		<script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
+	</head>
+<body>
+	<?php
+		include("./header.html");
+	?>
+
+	<div class="centra">
+		<section class="container">
+			<h1 class="titolo">Storico</h1>
+			<div class="storico">
+				<div class="riga_storia">
+					<div class="calendario">
+						<div class="giorno">
+							May
+						</div>
+						<div class="mese">
+							10
+						</div>
+					</div>
+					<div class="storia">
+						<div class="nome">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+						</div>
+						<a href="#" class="link_storia">Guarda le foto</a>
+					</div>
+				</div>
+
+				<div class="riga_storia">
+					<div class="calendario">
+						<div class="giorno">
+							May
+						</div>
+						<div class="mese">
+							10
+						</div>
+					</div>
+					<div class="storia">
+						<div class="nome">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+						</div>
+						<a href="#" class="link_storia">Guarda le foto</a>
+					</div>
+				</div>
+
+				<div class="riga_storia">
+					<div class="calendario">
+						<div class="giorno">
+							May
+						</div>
+						<div class="mese">
+							10
+						</div>
+					</div>
+					<div class="storia">
+						<div class="nome">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni
+						</div>
+						<a href="#" class="link_storia">Guarda le foto</a>
+					</div>
+				</div>
+
+				<div class="riga_storia">
+					<div class="calendario">
+						<div class="giorno">
+							May
+						</div>
+						<div class="mese">
+							10
+						</div>
+					</div>
+					<div class="storia">
+						<div class="nome">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+						</div>
+						<a href="#" class="link_storia">Guarda le foto</a>
+					</div>
+				</div>
+
+			</div>
+		</section>
+	</div>	
+		
+</body>
+</html>
