6d7107e50d22c5d2be54a9092f5ae6457ae15eb7
gerardomanzo <gerardo.manzo@outlook.com>
2016-11-02 22:05:12 +0100

    fixes

--- listastorico.php
+++ listastorico.php
@@ -11,78 +11,93 @@
 		include("./header.html");
 	?>
 
+	<nav class="menu">
+		<ul>
+			<li class="uno"><a href="index2.php">Home</a></li>
+			<li class="due"><a href="#">Storico</a></li>
+			<li class="tre"><a href="chisiamo.php">Chi siamo</a></li>
+			<li class="quattro"><a href="dovesiamo.php">Mappa</a></li>
+			<li class="cinque"><a href="contatti.php">Contatti</a></li>
+			<li class="sei"><a href="areaprivata.php">Area Privata</a></li>
+			<hr style="margin: 4px 0 0 16.6%" />
+		</ul>
+	</nav>
+
 	<div class="centra">
 		<section class="container">
-			<h1 class="titolo">Storico</h1>
 			<div class="storico">
-				<div class="riga_storia">
-					<div class="calendario">
-						<div class="giorno">
-							May
+				<h1 class="titolo">Storico</h1>
+				<div>
+					<div class="riga_storia">
+						<div class="calendario" style="float: left">
+							<div class="giorno">
+								May
+							</div>
+							<div class="mese">
+								10
+							</div>
 						</div>
-						<div class="mese">
-							10
+						<div class="storia">
+							<div class="nome">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+							</div>
+							<a href="#" class="link_storia">Guarda le foto</a>
 						</div>
+						<a class="bin" href="#"><img src="./images/bin.png"></a>
 					</div>
-					<div class="storia">
-						<div class="nome">
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-						</div>
-						<a href="#" class="link_storia">Guarda le foto</a>
-					</div>
-				</div>
 
-				<div class="riga_storia">
-					<div class="calendario">
-						<div class="giorno">
-							May
+					<div class="riga_storia">
+						<div class="calendario" style="float: left">
+							<div class="giorno">
+								May
+							</div>
+							<div class="mese">
+								10
+							</div>
 						</div>
-						<div class="mese">
-							10
+						<div class="storia">
+							<div class="nome">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+							</div>
+							<a href="#" class="link_storia">Guarda le foto</a>
 						</div>
 					</div>
-					<div class="storia">
-						<div class="nome">
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-						</div>
-						<a href="#" class="link_storia">Guarda le foto</a>
-					</div>
-				</div>
 
-				<div class="riga_storia">
-					<div class="calendario">
-						<div class="giorno">
-							May
+					<div class="riga_storia">
+						<div class="calendario" style="float: left">
+							<div class="giorno">
+								May
+							</div>
+							<div class="mese">
+								10
+							</div>
 						</div>
-						<div class="mese">
-							10
+						<div class="storia">
+							<div class="nome">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni
+							</div>
+							<a href="#" class="link_storia">Guarda le foto</a>
 						</div>
 					</div>
-					<div class="storia">
-						<div class="nome">
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni
-						</div>
-						<a href="#" class="link_storia">Guarda le foto</a>
-					</div>
-				</div>
 
-				<div class="riga_storia">
-					<div class="calendario">
-						<div class="giorno">
-							May
+					<div class="riga_storia">
+						<div class="calendario" style="float: left">
+							<div class="giorno">
+								May
+							</div>
+							<div class="mese">
+								10
+							</div>
 						</div>
-						<div class="mese">
-							10
+						<div class="storia">
+							<div class="nome">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+							</div>
+							<a href="#" class="link_storia">Guarda le foto</a>
 						</div>
 					</div>
-					<div class="storia">
-						<div class="nome">
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-						</div>
-						<a href="#" class="link_storia">Guarda le foto</a>
-					</div>
-				</div>
 
+				</div>
 			</div>
 		</section>
 	</div>	
